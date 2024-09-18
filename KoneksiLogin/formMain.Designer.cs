eba32d448a3ee512594b4f521771f2f3c554b9cb
Jordannst <jordansutarto8@gmail.com>
2024-09-18 19:46:01 +0800

    connection Login

---
+++ KoneksiLogin/formMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace KoneksiLogin
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(409, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anda Berhasil Login!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 651);
+            this.Controls.Add(this.label2);
+            this.Name = "formMain";
+            this.Text = "formMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+    }
+}
